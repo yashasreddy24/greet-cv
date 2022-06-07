4fd6a3cdc041b367014a0a3ff9c8fc09066df0d8
Yashas Reddy <yashas.reddy@neosoftmail.com>
2022-06-07 10:28:11 +0530

    fixing error which is raised when no human is present in video

--- main.py
+++ main.py
@@ -4,18 +4,29 @@ try:
     import numpy as np
     import cv2
     import mediapipe as mp
+    from os.path import dirname, abspath, join
 except ImportError as ie:
     print(ie)
+
+# project dir
+project_dir = abspath(dirname(__name__))
+media_dir = join(project_dir, 'media')
+input_file = join(media_dir, '01.mp4')
 
 mp_drawing = mp.solutions.drawing_utils
 mp_holistic = mp.solutions.holistic
 
+prev_zone = None
+curr_zone = None
+display = None
+window_name = "tracking window"
 
 try:
-    capt = cv2.VideoCapture(0)
+    capt = cv2.VideoCapture(input_file)
 
     width = int(capt.get(cv2.CAP_PROP_FRAME_WIDTH))
     height = int(capt.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    print(width, height)
 
     red_start = round((width / 100) * 20)
     red_end = round((width / 100) * 50)
@@ -31,14 +42,12 @@ try:
 
     font = cv2.FONT_HERSHEY_SIMPLEX
 
-    prev_zone = None
-    curr_zone = None
-    display = None
-
     writer = cv2.VideoWriter(filename='tracked video.mp4',
                              fourcc=cv2.VideoWriter_fourcc(*'mp4v'),
                              fps=25,
                              frameSize=(width, height))
+    cv2.namedWindow(window_name, cv2.WND_PROP_FULLSCREEN)
+    cv2.setWindowProperty(window_name, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 
     with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
 
@@ -49,42 +58,42 @@ try:
             try:
                 nose_x = results.pose_landmarks.landmark[mp_holistic.PoseLandmark.NOSE].x * width
                 nose_y = results.pose_landmarks.landmark[mp_holistic.PoseLandmark.NOSE].y * height
+
+                if (nose_x > red_start) & (nose_x < red_end):
+                    if curr_zone == None:
+                        curr_zone = 'red'
+                    else:
+                        if curr_zone == 'green':
+                            prev_zone = curr_zone
+                            curr_zone = 'red'
+
+                            display = "Good Bye!"
+
+                elif (nose_x > green_start) & (nose_x < green_end):
+                    if curr_zone == None:
+                        curr_zone = 'green'
+                    else:
+                        if curr_zone == 'red':
+                            prev_zone = curr_zone
+                            curr_zone = 'green'
+
+                            display = 'Welcome!'
+
+                else:
+                    prev_zone = curr_zone
+                    curr_zone = None
+                    display = None
             except:
                 pass
 
             # Drawing pose landmarks
             mp_drawing.draw_landmarks(frame, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
                                       mp_drawing.DrawingSpec(color=(255, 0, 0), thickness=2, circle_radius=1),
-                                      mp_drawing.DrawingSpec(color=(0,0,0),thickness=2,circle_radius=1)
+                                      mp_drawing.DrawingSpec(color=(0, 0, 0), thickness=2, circle_radius=1)
                                       )
 
-            if (nose_x > red_start) & (nose_x < red_end):
-                if curr_zone == None:
-                    curr_zone = 'red'
-                else:
-                    if curr_zone == 'green':
-                        prev_zone = curr_zone
-                        curr_zone = 'red'
-
-                        display = "Good Bye!"
-
-            elif (nose_x > green_start) & (nose_x < green_end):
-                if curr_zone == None:
-                    curr_zone = 'green'
-                else:
-                    if curr_zone == 'red':
-                        prev_zone = curr_zone
-                        curr_zone = 'green'
-
-                        display = 'Welcome!'
-
-            else:
-                prev_zone = curr_zone
-                curr_zone = None
-                display = None
-
             if display:
-                cv2.putText(frame, text=display, org=(10, 50), fontFace=font, fontScale=1, color=(0,0,0),
+                cv2.putText(frame, text=display, org=(10, 50), fontFace=font, fontScale=2, color=(0,0,0),
                             thickness=2, lineType=cv2.LINE_AA)
 
             # Red zone
@@ -96,7 +105,7 @@ try:
                                                            alpha=0.8, src2=green_arr, beta=0.2, gamma=0)
 
             writer.write(frame)
-            cv2.imshow('Webcam Feed', frame)
+            cv2.imshow(window_name, frame)
 
             if cv2.waitKey(10) & 0xFF == ord('q'):
                 break
